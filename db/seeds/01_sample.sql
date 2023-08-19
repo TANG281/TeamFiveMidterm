136877931b98b1590eab8fba633348f002c1eaad
Afreeda <64160666+afreeda-m@users.noreply.github.com>
2023-08-19 11:20:49 -0400

    update timestamp data

--- db/seeds/01_sample.sql
+++ db/seeds/01_sample.sql
@@ -1,14 +1,14 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name, email, password, phone_number)
-VALUES ('Alice', '1@email.com', '123', 123456789);
+VALUES ('Alice', '1@email.com', '123', NOW());
 INSERT INTO users (name, email, password, phone_number, is_admin)
-VALUES ('Kira', '2@email.com', '345', 987654321, TRUE);
+VALUES ('Kira', '2@email.com', '345', NOW(), TRUE);
 
 INSERT INTO items (owner_id, title, description, price, images_url, category, date_posted)
-VALUES (1, 'title', 'description', 150, './planning/images/sample.png', 'Phones', 1692370970);
+VALUES (1, 'title', 'description', 150, './planning/images/sample.png', 'Phones', NOW());
 INSERT INTO items (owner_id, title, description, price, images_url, category, date_posted)
-VALUES (2, 'title', 'description', 75, './planning/images/sample.png', 'Laptops', 1692370970);
+VALUES (2, 'title', 'description', 75, './planning/images/sample.png', 'Laptops', NOW());
 INSERT INTO items (owner_id, title, description, price, images_url, category, date_posted)
-VALUES (1, 'title', 'description', 500, './planning/images/sample.png', 'Mini PCs', 1692370970);
+VALUES (1, 'title', 'description', 500, './planning/images/sample.png', 'Mini PCs', NOW());
 INSERT INTO items (owner_id, title, description, price, images_url, category, date_posted)
-VALUES (2, 'title', 'description', 600, './planning/images/sample.png', 'Desktops', 1692370970);
+VALUES (2, 'title', 'description', 600, './planning/images/sample.png', 'Desktops', NOW());
