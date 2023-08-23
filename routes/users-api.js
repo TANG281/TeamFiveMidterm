604e3e1c0aa0781e561d662fbbf7697f62acbb30
Rora Alem <rora.goitom@gmail.com>
2023-08-23 14:23:20 -0400

    Add template vars to render add_edit template

--- routes/users-api.js
+++ routes/users-api.js
@@ -38,10 +38,6 @@ router.get('/login/:id', (req, res) => {
 });
 
 router.get('/logout', (req, res) => {
-  
-  res.clearCookie();
-  res.redirect('/');
-});
 
   res.clearCookie("is_admin");
   res.clearCookie("user_id");
@@ -107,7 +103,23 @@ router.get('/items/:item_id', (req, res) => {
 
 //Rendering add_item page for adding new items
 router.get('/items/add', (req, res) => {
-  res.render('add_edit');
+  // Extract user_id and is_admin from cookies
+  const user_id = req.cookies.user_id;
+  const is_admin = req.cookies.is_admin;
+
+  // Prepare template variables
+  const templateVars = {
+    user_id,
+    is_admin
+  };
+
+  if (!is_admin) {
+    res.redirect('/');
+  } else {
+
+  // Render the 'add_edit' template and pass template variables
+  res.render('add_edit', templateVars);
+  }
 });
 
 //Post route for form submission when creating new item
@@ -131,6 +143,7 @@ router.post('/items/create_new', (req, res) => {
   })
     .then(() => {
       // Redirect to the category page where the item was added
+      
       res.redirect('/category');
     })
       .catch(error => {
